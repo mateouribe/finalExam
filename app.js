741119e41452b608fa228c8878799524dc58eb29
Mateo Arismendy <arismendyuribemateo@gmail.com>
2023-08-15 08:14:10 -0400

    add valitaions to form and create object to be sent to db

--- app.js
+++ app.js
@@ -14,19 +14,17 @@ mongoose.connect("mongodb://localhost:27017/finalStore", {
 });
 
 const orderSchema = new mongoose.Schema({
-  //   name: String,
-  //   email: String,
-  //   phone: Number,
-  //   address: String,
-  //   city: String,
-  //   province: String,
-  //   postalCode: String,
-  //   watchQuantity: Number,
-  //   ipadQuantity: Number,
-  //   ipearQuantity: Number,
-  //   subtotal: Number,
-  //   taxes: Number,
-  //   total: Number,
+  name: String,
+  phone: Number,
+  address: String,
+  city: String,
+  province: String,
+  postalCode: String,
+  berryQuantity: Number,
+  appleQuantity: Number,
+  subtotal: Number,
+  taxes: Number,
+  total: Number,
 });
 
 // Create the Order model
@@ -62,80 +60,75 @@ app.get("/", (req, res) => {
 app.post("/submit", async (req, res) => {
   const orderData = req.body;
 
-  //   if (
-  //     !orderData.name ||
-  //     !orderData.email ||
-  //     !orderData.address ||
-  //     !orderData.postalCode ||
-  //     !orderData.province ||
-  //     !orderData.phone
-  //   ) {
-  //     return res
-  //       .status(400)
-  //       .send("Inputs with * are required. Please go back and try again.");
-  //   }
+  if (
+    !orderData.name ||
+    !orderData.address ||
+    !orderData.postalCode ||
+    !orderData.province ||
+    !orderData.phone ||
+    orderData.phone.length != 10 ||
+    isFinite(orderData.phone) == false
+  ) {
+    return res
+      .status(400)
+      .send("There are some errors in the form, pplease check again.");
+  }
 
-  // Calculate subtotal and taxes
-  //   const watchQuantity = parseInt(orderData.watch) || 0;
-  //   const ipadQuantity = parseInt(orderData.ipad) || 0;
-  //   const ipearQuantity = parseInt(orderData.ipear) || 0;
+  //   Calculate subtotal and taxes
+  const berryQuantity = parseInt(orderData.berry) || 0;
+  const appleQuantity = parseInt(orderData.apple) || 0;
 
-  //   const watchSubtotal = watchQuantity * 300;
-  //   const ipadSubtotal = ipadQuantity * 800;
-  //   const ipearSubtotal = ipearQuantity * 1000;
+  const berrySubtotal = berryQuantity * 10;
+  const appleSubtotal = appleQuantity * 20;
 
-  //   const subtotal = watchSubtotal + ipadSubtotal + ipearSubtotal;
-  //   const taxes = calculateTaxes(subtotal, orderData.province);
+  const subtotal = berrySubtotal + appleSubtotal;
+  const taxes = calculateTaxes(subtotal, orderData.province);
 
-  //   // Calculate total
-  //   const total = subtotal + taxes;
+  // Calculate total
+  const total = subtotal + taxes;
 
   //To show
-  //   const order = new Order({
-  //     name: orderData.name,
-  //     email: orderData.email,
-  //     phone: orderData.phone,
-  //     address: orderData.address,
-  //     city: orderData.city,
-  //     province: orderData.province,
-  //     postalCode: orderData.postalCode,
-  //     watchQuantity: orderData.watch,
-  //     ipadQuantity: orderData.ipad,
-  //     ipearQuantity: orderData.ipear,
-  //     subtotal: subtotal,
-  //     taxes: taxes,
-  //     total: total,
-  //   });
+  const order = new Order({
+    name: orderData.name,
+    phone: orderData.phone,
+    address: orderData.address,
+    city: orderData.city,
+    province: orderData.province,
+    postalCode: orderData.postalCode,
+    berryQuantity: orderData.berry,
+    appleQuantity: orderData.apple,
+    subtotal: subtotal,
+    taxes: taxes,
+    total: total,
+  });
 
-  //   try {
-  //     await order.save();
-  //     res.redirect(`/receipt/${order._id}`);
-  //   } catch (error) {
-  //     res.status(500).send("An error occurred while saving the order.");
-  //   }
-
-  //   res.send("Order submitted successfully!");
+  try {
+    await order.save();
+    res.redirect(`/success/${order._id}`);
+  } catch (error) {
+    res.status(500).send("An error occurred while saving the order.");
+  }
 });
 
-// app.get("/receipt/:orderId", async (req, res) => {
-//   const orderId = req.params.orderId;
-//   const order = await Order.findById(orderId);
+app.get("/success/:orderId", async (req, res) => {
+  const orderId = req.params.orderId;
+  const order = await Order.findById(orderId);
 
-//   if (!order) {
-//     return res.send("Order not found");
-//   }
+  if (!order) {
+    return res.send("Order not found");
+  }
 
-//   res.render("receipt", { order });
-// });
+  res.render("success", { order });
+});
 
-// app.get("/orders", async (req, res) => {
-//   try {
-//     const orders = await Order.find();
-//     res.render("orders", { orders });
-//   } catch (error) {
-//     res.status(500).send("An error occurred while fetching orders.");
-//   }
-// });
+app.get("/orders", async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.render("orders", { orders });
+  } catch (error) {
+    res.status(500).send("An error occurred while fetching orders.");
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
